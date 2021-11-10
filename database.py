d669744d9907c32ff40866b5b638bcec7cca41ac
raphael.baumeler <raphael.baumeler@ost.ch>
2021-11-10 15:01:08 +0100

    read Data

--- database.py
+++ database.py
@@ -1,10 +1,24 @@
 from pathlib import Path
 
-my_file = Path("database/myData.csv")
-
 
 def store_data(data):
-    if my_file.is_file():
-        data.to_csv(path_or_buf=my_file)
+    my_dir = Path("./database/")
+    my_file = Path("./database/myData.csv")
+    max_Size = 400  # 100 * 10 ** 6  # 100 MB
+
+    size = my_file.stat().st_size
+    if size < max_Size:
+        if my_dir.is_dir():
+            if my_file.is_file():
+                data.to_csv(path_or_buf=my_file, header=False, mode='a')
+            else:
+                data.to_csv(path_or_buf=my_file)
+        else:
+            my_dir.mkdir()
+            if my_file.is_file():
+                data.to_csv(path_or_buf=my_file, header=False, mode='a')
+            else:
+                data.to_csv(path_or_buf=my_file)
+        print("Database not Full")
     else:
-        data.to_csv(path_or_buf=my_file, header=False, mode='a')
+        print("Database is Full")
