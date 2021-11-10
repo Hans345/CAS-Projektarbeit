ce986bb830467b3ccb277c7e285b8ee145a59773
raphael.baumeler <raphael.baumeler@ost.ch>
2021-11-10 12:53:00 +0100

    read Data

--- modbus.py
+++ modbus.py
@@ -76,8 +76,8 @@ def getDataRow(instrument, adrSpace, s):
 
         # save to  Data Frame
         df = pd.DataFrame(data, columns=s)
-        df = df.assign(time=dt.datetime.now())
-        df = df.set_index('time', drop=True)
+        # df = df.assign(time=dt.datetime.now())
+        # df = df.set_index('time', drop=True)
     else:
         print('Inputparameter müssen die gleiche Grösse haben!')
 
@@ -93,54 +93,66 @@ def delDataCol(data, s):
     """
     return data.drop(columns=s)
 
+def getData():
+    """
+    :return: type: DataFrame
+    """
+    # init Modbus
+    port = '/dev/ttyUSB0'
+    adr = 1
+    modBus = initModbus(port, adr)
+
+    # calc Register Addresses
+    adrSpace1 = getAdrSpace("5000", "5030", "02")
+    adrSpace2 = getAdrSpace("6006", "600A", "02")
+    adrSpace3 = getAdrSpace("602A", "602E", "02")
+
+    # read Data and Store to pandas Dataframe
+    s1 = (["V", "V_L1", "V_L2", "V_L3", "freq", "I", "I_L1", "I_L2", "I_L3", "p_sum", "p_L1", "p_L2", "p_L3", "q_sum",
+           "q_L1", "q_L2", "q_L3", "s_sum", "s_L1", "s_L2", "s_L3", "pf", "pf_L1", "pf_L2", "pf_L3"])
+    s2 = (["eAct_L1", "eAct_L2", "eAct_L3"])
+    s3 = (["eReact_L1", "eReact_L2", "eReact_L3"])
+    data1 = getDataRow(modBus, adrSpace1, s1)
+    data2 = getDataRow(modBus, adrSpace2, s2)
+    data3 = getDataRow(modBus, adrSpace3, s3)
+
+    # clear unrelevant Data Cols
+    sDel = (["V", "I"])
+    data1 = delDataCol(data1, sDel)
+
+    # final DataRow
+    data = pd.concat([data1, data2, data3], axis=1)
+    data = data.assign(time=dt.datetime.now())
+    data = data.set_index('time', drop=True)
+
+    return data
 
 
-# init Modbus
-port = '/dev/ttyUSB0'
-adr = 1
-modBus = initModbus(port,adr)
-
-# calc Register Addresses
-adrSpace1 = getAdrSpace("5000", "5030", "02")
-adrSpace2 = getAdrSpace("6006", "600A", "02")
-adrSpace3 = getAdrSpace("602A", "602E", "02")
-
-# read Data and Store to pandas Dataframe
-s1 = (["V", "V_L1", "V_L2", "V_L3", "freq", "I", "I_L1", "I_L2", "I_L3", "p_sum", "p_L1", "p_L2", "p_L3", "q_sum", "q_L1",
-      "q_L2", "q_L3", "s_sum", "s_L1", "s_L2", "s_L3", "pf", "pf_L1", "pf_L2", "pf_L3"])
-s2 = (["eAct_L1", "eAct_L2", "eAct_L3"])
-s3 = (["eReact_L1", "eReact_L2", "eReact_L3"])
-data1 = getDataRow(modBus, adrSpace1, s1)
-data2 = getDataRow(modBus, adrSpace2, s2)
-data3 = getDataRow(modBus, adrSpace3, s3)
-
-# clear unrelevant Data Cols
-sDel = (["V", "I"])
-data1 = delDataCol(data1, sDel)
 
 # print all
-print("##########")
-print("Addressraum 1:\n" + str(adrSpace1))
-print("##########")
-print("Addressraum 2:\n" + str(adrSpace2))
-print("##########")
-print("Addressraum 3:\n" + str(adrSpace3))
-print("##########")
-print("##########")
-print("Data 1: \n" + str(data1.dtypes))
-print("Data 1: \n" + str(data1))
-print("##########")
-print("Data 2: \n" + str(data2.dtypes))
-print("Data 2: \n" + str(data2))
-print("##########")
-print("Data 3: \n" + str(data3.dtypes))
-print("Data 3: \n" + str(data3))
+# print("##########")
+# print("Addressraum 1:\n" + str(adrSpace1))
+# print("##########")
+# print("Addressraum 2:\n" + str(adrSpace2))
+# print("##########")
+# print("Addressraum 3:\n" + str(adrSpace3))
+# print("##########")
+# print("##########")
+# print("Data 1: \n" + str(data1.dtypes))
+# print("Data 1: \n" + str(data1))
+# print("##########")
+# print("Data 2: \n" + str(data2.dtypes))
+# print("Data 2: \n" + str(data2))
+# print("##########")
+# print("Data 3: \n" + str(data3.dtypes))
+# print("Data 3: \n" + str(data3))
 
 # test = pd.DataFrame([[data1.iloc[0]["V_L3"], data1.iloc[0]["I_L3"], data1.iloc[0]["pf_L3"], data1.iloc[0]["p_L3"],
 #                       data2.iloc[0]["eAct_L3"],
 #                       data3.iloc[0]["eReact_L3"]]])
 # print("Relevant Data: \n" + str(test))
 
+print("Final DataRow: \n" + str(getData()))
 
 
 
