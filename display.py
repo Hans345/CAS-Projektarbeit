f22948fe8e42d5e8a7b567a6e3aa84f634b82d77
raphael.baumeler <raphael.baumeler@ost.ch>
2021-11-15 15:36:56 +0100

    display

--- display.py
+++ display.py
@@ -47,6 +47,8 @@ class PiOLED:
     def set_string(self, s):
         # Clear old text
         self.draw.text((0, -2 + 25), '', font=self.font, fill=255)
+        self.disp.image(self.image)
+        self.disp.show()
 
         # Draw new text
         self.draw.text((0, -2 + 25), s, font=self.font, fill=255)
