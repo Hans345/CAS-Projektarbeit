dedb50c2e07869622c5d8eaa2f9a2b20fd0866a7
raphael.baumeler <raphael.baumeler@ost.ch>
2021-11-29 15:06:31 +0100

    django

--- webApplication/dataVisualisation/views.py
+++ webApplication/dataVisualisation/views.py
@@ -25,20 +25,20 @@ def test(request):
     # Display
     miniDisplay = PiOLED()
 
-    # create new database with maxsize
-    for i in range(10):
-        if (size_csv < max_Size) or (size_sqlite < max_Size):
-            dataRow = get_data()
-            data = data.append(dataRow)
-            size_csv = store_data_csv(dataRow, path_csv)  # store to .csv
-            size_sqlite = store_data_sqlite3(dataRow, path_sqlite)  # store to .sqlite
-        else:
-            print("Database is full: " + str(size_csv) + " Bytes")
-            break
-        if i < 1:
-            miniDisplay.set_string("Messung läuft !")
-    miniDisplay.set_string("Messung gestoppt !")
-
-    print(data.head())
+    # # create new database with maxsize
+    # for i in range(10):
+    #     if (size_csv < max_Size) or (size_sqlite < max_Size):
+    #         dataRow = get_data()
+    #         data = data.append(dataRow)
+    #         size_csv = store_data_csv(dataRow, path_csv)  # store to .csv
+    #         size_sqlite = store_data_sqlite3(dataRow, path_sqlite)  # store to .sqlite
+    #     else:
+    #         print("Database is full: " + str(size_csv) + " Bytes")
+    #         break
+    #     if i < 1:
+    #         miniDisplay.set_string("Messung läuft !")
+    # miniDisplay.set_string("Messung gestoppt !")
+    #
+    # print(data.head())
 
     return HttpResponse("Init Done!")
