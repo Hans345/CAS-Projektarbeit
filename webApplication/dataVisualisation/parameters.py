31962db0fd4aa3dd17f6927fbffadfb949cfd7a1
raphael.baumeler <raphael.baumeler@ost.ch>
2021-11-29 15:10:28 +0100

    django

--- webApplication/dataVisualisation/parameters.py
+++ webApplication/dataVisualisation/parameters.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 # database Param
 max_Size = 1000  # max Size (Byte)
-path_csv = Path("./database/myData.csv")
-path_sqlite = Path("./database/myData.sqlite")
+path_csv = Path("C:/CAS_EnD_HS21/06_Projektarbeit/02_Code/database/myData.csv")
+path_sqlite = Path("C:/CAS_EnD_HS21/06_Projektarbeit/02_Code/database/myData.sqlite")
 
 # modbus Param
 data = pd.DataFrame()
