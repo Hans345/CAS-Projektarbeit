940880e1fa79549c38f11350544a5e879ca0307c
raphael.baumeler <raphael.baumeler@ost.ch>
2021-12-08 14:00:56 +0100

    display

--- testApplication/display.py
+++ testApplication/display.py
@@ -1,42 +1,85 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  8 13:47:50 2021
+# SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
+# SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
-@author: raphael.baumeler
-"""
+# This example is for use on (Linux) computers that are using CPython with
+# Adafruit Blinka to support CircuitPython libraries. CircuitPython does
+# not support PIL/pillow (python imaging library)!
 
-# -*- coding: utf-8 -*
-"""
-Python-Code welcher bei Start ausgeführt wird
-@author: markus markstaler
-"""
-import datetime as dt
 import time
+import subprocess
+
+from board import SCL, SDA
 import busio
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
-import subprocess
-import socket
-# Initalize Display
-i2c = busio.I2C(3, 2) # Create the I2C with SCL = 3, SDA = 2.
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c) # Create class
-image = Image.new('1', (128, 32)) # Create blank image for drawing.
-draw = ImageDraw.Draw(image) # Get drawing object to draw on image
-font = ImageFont.load_default() # Load default font.
+
+
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+# Clear display.
+disp.fill(0)
+disp.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new("1", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
+
 while True:
-    zeit = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # Ermittelt IP-Adresse
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # erstellt Netzwerkverbindung
-    s.connect(('1.1.1.1',1)) # fiktiver Internetzugriff
-    ip = s.getsockname()[0] # ermittelt IP-Adresse
-    # Display
-    draw.rectangle((0, 0, 128, 32), outline=0, fill=0) # Draw box to clear image
-    draw.text((0, -2+0), 'IP: %s' %ip, font=font, fill=255)
-    draw.text((0, -2+8), zeit, font=font, fill=255)
-    draw.text((0, -2+16), '', font=font, fill=255)
-    draw.text((0, -2+25), 'es funktioniert !', font=font, fill=255)
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here:
+    # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d' ' -f1"
+    IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'cut -f 1 -d " " /proc/loadavg'
+    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
+    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
+
+    # Write four lines of text.
+
+    draw.text((x, top + 0), "IP: " + IP, font=font, fill=255)
+    draw.text((x, top + 8), "CPU load: " + CPU, font=font, fill=255)
+    draw.text((x, top + 16), MemUsage, font=font, fill=255)
+    draw.text((x, top + 25), Disk, font=font, fill=255)
+
+    # Display image.
     disp.image(image)
     disp.show()
-    print(ip)
-    print(zeit)
-    time.sleep(5)
+    time.sleep(0.1)
