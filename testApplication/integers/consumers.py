aef12cf3db859ab6ed59446969cbcc23958b573b
raphael.baumeler <raphael.baumeler@ost.ch>
2021-12-09 09:59:58 +0100

    display

--- testApplication/integers/consumers.py
+++ testApplication/integers/consumers.py
@@ -37,52 +37,52 @@ class WSConsumer(WebsocketConsumer):  # subclass from WebsocketConsumer class
 
         i = 0
         # create new database with maxsize
-        while 1:
-            # get data from PRO380-Mod
-            self.dataRow = get_data()
-            # update database
-            if (self.size_csv < self.max_Size) or (self.size_sqlite < self.max_Size):
-                self.size_csv = store_data_csv(self.dataRow, self.path_csv)  # store to .csv
-                self.size_sqlite = store_data_sqlite3(self.dataRow, self.path_sqlite)  # store to .sqlite
-            else:
-                if i == 0:
-                    print("Database is full: " + str(self.size_csv) + " Bytes")
-                    self.miniDisplay.set_string("Stop Log Data!")
-                    i = 1
-            # update webpage
-            self.send(json.dumps({'VL1': float(self.dataRow["V_L1"]),
-                                  'VL2': float(self.dataRow["V_L2"]),
-                                  'VL3': float(self.dataRow["V_L3"]),
-                                  'VMean': np.round((float(self.dataRow["V_L1"]) + float(self.dataRow["V_L2"]) + float(
-                                      self.dataRow["V_L3"])) / 3, decimals=3),
-                                  'IL1': float(self.dataRow["I_L1"]),
-                                  'IL2': float(self.dataRow["I_L2"]),
-                                  'IL3': float(self.dataRow["I_L3"]),
-                                  'IMean': np.round((float(self.dataRow["I_L1"]) + float(self.dataRow["I_L2"]) + float(
-                                      self.dataRow["I_L3"])) / 3, decimals=3),
-                                  'PF1': float(self.dataRow["pf_L1"]),
-                                  'PF2': float(self.dataRow["pf_L2"]),
-                                  'PF3': float(self.dataRow["pf_L3"]),
-                                  'PFMean': float(self.dataRow["pf"]),
-                                  'p1': float(self.dataRow["p_L1"]),
-                                  'p2': float(self.dataRow["p_L2"]),
-                                  'p3': float(self.dataRow["p_L3"]),
-                                  'pSum': float(self.dataRow["p_sum"]),
-                                  'q1': float(self.dataRow["q_L1"]),
-                                  'q2': float(self.dataRow["q_L2"]),
-                                  'q3': float(self.dataRow["q_L3"]),
-                                  'qSum': float(self.dataRow["q_sum"]),
-                                  's1': float(self.dataRow["s_L1"]),
-                                  's2': float(self.dataRow["s_L2"]),
-                                  's3': float(self.dataRow["s_L3"]),
-                                  'sSum': float(self.dataRow["s_sum"]),
-                                  'eA1': float(self.dataRow["eAct_L1"]),
-                                  'eA2': float(self.dataRow["eAct_L2"]),
-                                  'eA3': float(self.dataRow["eAct_L3"]),
-                                  'eASum': float(self.dataRow["eAct_Tot"]),
-                                  'eR1': float(self.dataRow["eReact_L1"]),
-                                  'eR2': float(self.dataRow["eReact_L2"]),
-                                  'eR3': float(self.dataRow["eReact_L3"]),
-                                  'eRSum': float(self.dataRow["eReact_Tot"]),
-                                  'freq': float(self.dataRow["freq"]),
-                                  }))
+        # while 1:
+        # get data from PRO380-Mod
+        self.dataRow = get_data()
+        # update database
+        if (self.size_csv < self.max_Size) or (self.size_sqlite < self.max_Size):
+            self.size_csv = store_data_csv(self.dataRow, self.path_csv)  # store to .csv
+            self.size_sqlite = store_data_sqlite3(self.dataRow, self.path_sqlite)  # store to .sqlite
+        else:
+            if i == 0:
+                print("Database is full: " + str(self.size_csv) + " Bytes")
+                self.miniDisplay.set_string("Stop Log Data!")
+                i = 1
+        # update webpage
+        self.send(json.dumps({'VL1': float(self.dataRow["V_L1"]),
+                              'VL2': float(self.dataRow["V_L2"]),
+                              'VL3': float(self.dataRow["V_L3"]),
+                              'VMean': np.round((float(self.dataRow["V_L1"]) + float(self.dataRow["V_L2"]) + float(
+                                  self.dataRow["V_L3"])) / 3, decimals=3),
+                              'IL1': float(self.dataRow["I_L1"]),
+                              'IL2': float(self.dataRow["I_L2"]),
+                              'IL3': float(self.dataRow["I_L3"]),
+                              'IMean': np.round((float(self.dataRow["I_L1"]) + float(self.dataRow["I_L2"]) + float(
+                                  self.dataRow["I_L3"])) / 3, decimals=3),
+                              'PF1': float(self.dataRow["pf_L1"]),
+                              'PF2': float(self.dataRow["pf_L2"]),
+                              'PF3': float(self.dataRow["pf_L3"]),
+                              'PFMean': float(self.dataRow["pf"]),
+                              'p1': float(self.dataRow["p_L1"]),
+                              'p2': float(self.dataRow["p_L2"]),
+                              'p3': float(self.dataRow["p_L3"]),
+                              'pSum': float(self.dataRow["p_sum"]),
+                              'q1': float(self.dataRow["q_L1"]),
+                              'q2': float(self.dataRow["q_L2"]),
+                              'q3': float(self.dataRow["q_L3"]),
+                              'qSum': float(self.dataRow["q_sum"]),
+                              's1': float(self.dataRow["s_L1"]),
+                              's2': float(self.dataRow["s_L2"]),
+                              's3': float(self.dataRow["s_L3"]),
+                              'sSum': float(self.dataRow["s_sum"]),
+                              'eA1': float(self.dataRow["eAct_L1"]),
+                              'eA2': float(self.dataRow["eAct_L2"]),
+                              'eA3': float(self.dataRow["eAct_L3"]),
+                              'eASum': float(self.dataRow["eAct_Tot"]),
+                              'eR1': float(self.dataRow["eReact_L1"]),
+                              'eR2': float(self.dataRow["eReact_L2"]),
+                              'eR3': float(self.dataRow["eReact_L3"]),
+                              'eRSum': float(self.dataRow["eReact_Tot"]),
+                              'freq': float(self.dataRow["freq"]),
+                              }))
