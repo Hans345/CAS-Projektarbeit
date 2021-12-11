3be445e45540f951eb122d73495c4042be631f03
raphael.baumeler <raphael.baumeler@ost.ch>
2021-12-11 12:01:36 +0100

    IP

--- testApplication/integers/views.py
+++ testApplication/integers/views.py
@@ -1,6 +1,10 @@
+import socket
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', context={'text': '0'})
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+
+    return render(request, 'index.html', context={'text': '0', 'ipAddress': local_ip})
