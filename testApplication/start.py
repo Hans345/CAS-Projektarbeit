e9dcc7b9a2bcd2b879757a4fb2535000b6b3e52e
raphael.baumeler <raphael.baumeler@ost.ch>
2021-12-11 12:20:39 +0100

    IP

--- testApplication/start.py
+++ testApplication/start.py
@@ -69,7 +69,7 @@ disp.show()
 
 # Start Webpage
 # subprocess.Popen(['python3', 'manage.py', 'migrate'])
-# subprocess.Popen(['python3', 'manage.py', 'runserver', ip+':8000'])
+subprocess.Popen(['python3', 'manage.py', 'runserver', ip+':8000'])
 # subprocess.Popen(['python3', 'manage.py', 'runserver', '127.0.0.1:8000'])
 
 time.sleep(1)
