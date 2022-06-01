4fa66b5dfc49208abf05cb34b7c1db33e5a0982d
raphael.baumeler <raphael.baumeler@ost.ch>
2022-06-01 16:31:15 +0200

    secondEnergyLogger_bugfix

--- testApplication/integers/modbus.py
+++ testApplication/integers/modbus.py
@@ -39,7 +39,7 @@ def init_modbus(prt, slave_adr):
     # read Data and Store to pandas Dataframe
     s1 = (["V", "V_L1", "V_L2", "V_L3", "freq", "I", "I_L1", "I_L2", "I_L3", "p_sum", "p_L1", "p_L2", "p_L3", "q_sum",
            "q_L1", "q_L2", "q_L3", "s_sum", "s_L1", "s_L2", "s_L3", "pf", "pf_L1", "pf_L2", "pf_L3"])
-    data1 = get_data_row(mod_bus, adr_space1, s1)
+    data1 = get_data_row(instrument, adr_space1, s1)
     if data1[0] != 0:
         print("Voltage L1 received: " + data1["V_L1"])
     else:
